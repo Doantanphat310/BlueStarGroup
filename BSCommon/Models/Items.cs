f243653958b402b2cae9625dfa989470b3677e85
thiensunhien <ngoctung1210@gmail.com>
2020-11-24 08:47:23 +0700

    add ItemSpecification to ItemList

--- BSCommon/Models/Items.cs
+++ BSCommon/Models/Items.cs
@@ -11,6 +11,7 @@ namespace BSCommon.Models
         public string ItemID { get; set; }
         public string ItemName { get; set; }
         public string ItemSName { get; set; }
+        public string ItemSpecification { get; set; }
         public string ItemTypeID { get; set; }
         public string ItemUnit { get; set; }
         public int? IsDelete { get; set; }
