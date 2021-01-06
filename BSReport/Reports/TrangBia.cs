024eda62b2572bd99778da1bc197c9725b8539e8
thiensunhien <ngoctung1210@gmail.com>
2021-01-06 20:35:56 +0700

    thêm report trang bìa

---
+++ BSReport/Reports/TrangBia.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.XtraReports.UI;
+
+namespace BSReport.Reports
+{
+    public partial class TrangBia : DevExpress.XtraReports.UI.XtraReport
+    {
+        public TrangBia()
+        {
+            InitializeComponent();
+        }
+    }
+}
