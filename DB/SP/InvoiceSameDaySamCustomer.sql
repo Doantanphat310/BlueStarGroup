e303ce46f03bd4d97642dca9d7c50ef713303d7f
Doantanphat310 <Doantanphat310@gmail.com>
2021-03-01 06:00:59 +0700

    Cập nhật invoice và các tiện ích

--- DB/SP/InvoiceSameDaySamCustomer.sql
+++ DB/SP/InvoiceSameDaySamCustomer.sql
@@ -3,5 +3,7 @@ alter proc InvoiceSameDaySamCustomer
 as
 begin
 --select * from View_InvoiceSameDaySamCustomer where CompanyID = @CompanyID
-select * from Invoice where CompanyID = @CompanyID and IsDelete is null
+select * from Invoice where CompanyID = @CompanyID and IsDelete is null and S35Type is null
 end
+
+
